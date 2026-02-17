dfb2c7095301b1cf5176b1215b59f68b990d2ffc
Jordys <ddamasojordys@gmail.com>
2026-02-16 19:48:18 -0600

    Agregando la clase Prueba

---
+++ HolaMundo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
