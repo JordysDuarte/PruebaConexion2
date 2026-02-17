2a2de2ed4415ece56cd825522376e1fbbaee7cd9
Jordys <ddamasojordys@gmail.com>
2026-02-16 19:56:37 -0600

    Agregando la clase suma

---
+++ HolaMundo/Suma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+     class Suma
+    {
+        
+        public class Operacion (int num1, int num2)
+        {
+            int num1 = 34;
+            int num2 = 55;
+
+            int resultado = num1 + num2;
+
+            
+        }
+
+    }
+}
